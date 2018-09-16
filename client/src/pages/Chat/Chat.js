61cfd6b26da8c5eb9607e2562a2f9cf6e9b9677e
max77p <shanindrakumar@gmail.com>
2018-09-15 22:32:50 -0400

    redux fixed

--- client/src/pages/Chat/Chat.js
+++ client/src/pages/Chat/Chat.js
@@ -1,25 +1,20 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { connect } from "react-redux";
+import { Container, Row, Col } from "../../components/Grid";
 
 class Chat extends Component {
-
   render() {
     return (
-      <div class="container">
-        <div>{props.showuser}</div>
-        
-            {/* <span class="flow-text">
-              This div is 12-columns wide on all screen sizes
-            </span> */}
-        
-      </div>
+      <Container>
+        <div>{this.props.ctr}</div>
+      </Container>
     );
   }
-
 }
 
-const mapStateToProps = state => {//get state from global .js aka reducer file
+const mapStateToProps = state => {
+  //get state from global .js aka reducer file
   return {
     ctr: state.showuser
   };
