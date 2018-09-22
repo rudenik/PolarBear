bba91490b2640407acc76f8086c2268a652e7745
max77p <shanindrakumar@gmail.com>
2018-09-22 11:53:06 -0400

    layout complete, styling still to do

--- client/src/pages/Chat/Chat.js
+++ client/src/pages/Chat/Chat.js
@@ -1,15 +1,48 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import axios from "axios";
+import { connect } from "react-redux";
+import { Row, Col, Container } from "../../components/Grid";
+import MaterialIcon, { colorPalette } from "material-icons-react";
+import "./Chat.css";
 
 class Chat extends Component {
-    render(){
+  render() {
+    return [
+      <Container customClass="mainChatContainer">
+        <div class="col s12 m12 userBar">
+          <div className="userPhoto">
+            <img
+              src="http://www.placepuppy.net/1p/200/200"
+              alt=""
+              className="headShot"
+            />
+          </div>
+          <div className="userInfo">
+            <p>{this.props.name}</p>
+            <p>{this.props.title}</p>
+          </div>
+          <div className="icons">
+            <MaterialIcon icon="account_circle" color="black" />
+            <MaterialIcon icon="message" />
+          </div>
+        </div>
 
-        return([
-            <div>Hello</div>,
-            <div>Hello</div>,
-            <div>Hello</div>,
-        ])
-    }
+        <div className="chatArea">blah blah</div>
+        <div class="type_msg">
+          <div class="input_msg_write">
+            <input type="text" class="write_msg" placeholder="Type a message" />
+          </div>
+        </div>
+      </Container>
+    ];
+  }
 }
 
-export default Chat;
+const mapStateToProps = state => {
+  return {
+    name: state.userName,
+    title: state.userTitle
+  };
+};
+
+export default connect(mapStateToProps)(Chat);
