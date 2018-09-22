2c5e1ed93feb5c5ffd040088edd6eef9627b451e
TrikaMit <trikamitra@gmail.com>
2018-09-22 11:37:49 -0400

    merge conflics with BEM format

--- client/src/pages/MatchCard/card/card.js
+++ client/src/pages/MatchCard/card/card.js
@@ -4,11 +4,7 @@ import "./card.css";
 class Card extends Component {
     render () {
         return (
-<<<<<<< HEAD
-            <div className="row center-align valign-wrapper card">
-=======
             <div className="row center-align valign-wrapper match__card">
->>>>>>> dev
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
             </div>
         )
