06054f062a5fd9029196da695c0b37e7c88e691a
Aaron Colbert <aaron-colbert@hotmail.com>
2018-09-22 10:59:51 -0400

    added

---
+++ client/src/pages/Event/Event.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import "./Event.css"
+import { InputText, InputArea } from "../../components/event__form";
+import { Row, Col, Container } from "../../components/Grid";
+import {Button, Dropdown, NavItem} from 'react-materialize'
+
+class Event extends Component {
+
+    state = {
+        name: "Johnny",
+        eventCode: "",
+        choice:""
+    };
+
+
+     render() {
+   return (
+     <div className="Event">
+     <Container>
+       <div>
+        <p>Hi {this.state.name}!</p> 
+         <InputArea className="event__code" label="Event Code Name" textAreaID="eventCode" onChange={this.handleInputChange}/>
+       </div>
+        <div className="JobSeeker">
+          <p>I am a:</p> 
+          <Dropdown trigger={
+        <Button>Job Seeker</Button>
+            }>
+         <NavItem>Student</NavItem>
+         <NavItem>Employer</NavItem>
+         <NavItem>Job Seeker</NavItem>
+        </Dropdown>
+        </div>
+         <button type="button" className="waves-effect waves-light btn event__btn" onClick={this.handleSignUpButton}>Sign Up for Event </button> 
+      
+         </Container>
+     </div>
+   );
+ }
+
+}
+
+export default Event;
---
+++ client/src/pages/MatchCard/card/card.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import "./card.css";
+
+class Card extends Component {
+    render () {
+        return (
+            <div className="row center-align valign-wrapper card">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            </div>
+        )
+        // TODO: add carousel
+    }
+}
+
+export default Card;
