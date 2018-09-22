b82003e09b8f3aa71d52f477643278c78e257f7b
Aaron Colbert <aaron-colbert@hotmail.com>
2018-09-22 11:25:52 -0400

    conflicts?

--- client/src/pages/MatchCard/header/header.js
+++ client/src/pages/MatchCard/header/header.js
@@ -4,19 +4,11 @@ import "./header.css";
 class Header extends Component {
     render () {
         return (
-<<<<<<< HEAD
-            <div className="row center-align header">
-                <div className='col s2'> 
-                    Pic
-                </div>
-                <div className='col s8 center-align title'>
-=======
             <div className="row center-align match__header">
                 <div className='col s2'> 
                     Pic
                 </div>
                 <div className='col s8 center-align match__title'>
->>>>>>> dev
                     Polar Bear
                 </div>
                 <div className='col s2 center-align'>
