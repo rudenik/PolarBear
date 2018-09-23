f8101db477a3d128c53321e6e3f7d18e22bd8362
Simon Cain <rudenik@gmail.com>
2018-09-23 17:01:28 -0400

    fixed a few things on the text input

--- client/src/components/Form/InputText.js
+++ client/src/components/Form/InputText.js
@@ -1,16 +1,16 @@
 import React from "react";
-import MaterialIcon, {colorPalette} from 'material-icons-react';
+// import MaterialIcon, {colorPalette} from 'material-icons-react';
 import { Row, Col } from "../Grid";
+import "./InputText.css";
 
 
 export const InputText = (props) => {
 return(
     <div className="input-field">
         <Row>
-        <Col size="s1">
-        <MaterialIcon icon={props.matIcon} color="#2667FF"/>
-        </Col>
+        
         <Col size="s9">
+        <i className="prefix material-icons" style={{color:"#2667FF" }}>{props.matIcon}</i>
         <input id={props.inputID} type="text" className="validate" />
         <label>{props.fieldName}</label>
         </Col>
@@ -18,3 +18,5 @@ return(
     </div>
 )
 }
+
+//<MaterialIcon icon={props.matIcon} color="#2667FF"/>
