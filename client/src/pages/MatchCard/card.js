e975b08c4a25abc0862766779b283c899056358f
TrikaMit <trikamitra@gmail.com>
2018-09-25 02:41:21 -0400

    added carousel functionality, still CSS issues to fix.

--- client/src/pages/MatchCard/card.js
+++ client/src/pages/MatchCard/card.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
+import { Carousel, CardPanel, Col } from 'react-materialize';
 import "./card.css";
 
 
 class Card extends Component {
-    render (props) {
-        return (
-            <div className="row center-align valign-wrapper match__card">
-            {this.props.cardOne}
-            </div>
-        )
-        // TODO: add carousel
-    }
+render (props) {
+return (
+<div className="row match__card ">
+<div className="match__carousel">
+    <Carousel options={{ fullWidth: true, indicators:true }}>
+        <div>
+            <p>{this.props.cardOne}</p>
+        </div>
+        <div>
+            <p>{this.props.cardTwo}</p>
+        </div>
+        <div>
+            <p>{this.props.cardThree}</p>
+        </div>
+    </Carousel>
+    </div>
+</div>
+
+
+
+)
+// TODO: fix carousel CSS
+//TODO: fix so that on "pass/connect" it navigates to first carousel item 
+//TODO: might have to scrap carousel idea all together and do from scratch.
+}
 }
 
 export default Card;
