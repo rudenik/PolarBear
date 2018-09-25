5fbc615ccc0526d845595bf275b45cebe44b716b
Simon Cain <rudenik@gmail.com>
2018-09-24 20:14:16 -0400

    added the google auth to the landing page. Needs to be modified but it's there.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,5 +9,11 @@ router
     .get(userController.findById)
     // .put(articleController.update)
     .delete(userController.remove);
+router.route("/redirect")
+        .get(function (req, res){
+            console.log(req.body);
+            res.json("redirect hit")
+        });
+    
 
 module.exports = router; 
