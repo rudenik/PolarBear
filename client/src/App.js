91e13f725fb2261c306a4a747ff91e03de61cce5
Adriana Shulman <adriana33@mac.com>
2018-09-24 21:14:23 -0400

    styling updates

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,9 @@ import Chat from "./pages/Chat";
 import SignUp from "./pages/SignUp";
 import Event from "./pages/Event";
 import Landing from "./pages/Landing";
-<<<<<<< HEAD
-import YourAccount from "./pages/YourAccount";
-
-=======
 import MatchCard from "./pages/MatchCard"
 import YourAccount from "./pages/YourAccount"
 import UserMatches from "./pages/UserMatches";
->>>>>>> aeb88281d547cb0e2e025f98951effcb26497786
 
 
 
@@ -28,13 +23,9 @@ class App extends Component {
       <Route exact path="/chat" component={Chat} />
       <Route exact path="/event" component={Event} /> 
       <Route exact path="/signup" component={SignUp} />
-<<<<<<< HEAD
-      <Route exact path="/YourAccount" component={YourAccount} />
-=======
       <Route exact path="/youraccount" component={YourAccount} />
       <Route exact path="/match" component={MatchCard} />
       <Route exact path="/usermatches" component={UserMatches} />
->>>>>>> aeb88281d547cb0e2e025f98951effcb26497786
       </Switch>
       </Router> 
       </div>
--- client/src/pages/Event/Event.js
+++ client/src/pages/Event/Event.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import "./Event.css"
 import { InputText, InputArea } from "../../components/event__form";
 import { Row, Col, Container } from "../../components/Grid";
-// import {Button, Dropdown, NavItem} from 'react-materialize'
+
 
 class Event extends Component {
 
@@ -13,20 +13,30 @@ class Event extends Component {
     };
 
 
-     render() {
+     render() {       
    return (
      <div className="Event">
      <Container>
-       <div>
-        <p>Hi {this.state.name}!</p> 
-         <InputArea className="event__code" label="Event Code Name" textAreaID="eventCode" onChange={this.handleInputChange}/>
-       </div>
-        <div className="JobSeeker">
-          <p>I am a:</p> 
-          <button type="button" className="waves-effect waves-light btn event__btn" onClick={this.handleSignUpButton}>Pick One (dropdown)</button> 
-        </div>
-         <button type="button" className="waves-effect waves-light btn event__btn" onClick={this.handleSignUpButton}>Sign Up for Event </button> 
       
+        <p>Hi {this.state.name}!</p> 
+        <br/>
+       <InputText id="event__code" fieldName="Event Code" inputId="eventCode" matIcon="event" onChange={this.handleInputChange}/>
+        <div className="JobSeeker">
+          <p className="Event__left" >I am a:</p> 
+          <label for="jobStatus" className="visuallyhidden">Job Status</label>
+
+                            <select name="eventStatus" id="eventStatus" >
+                                <option value = "jobSeeker"
+                                className="event__align-center">Job Seeker</option>
+                                <option value="youraccount__hiringManager">Hiring Manager</option>
+                                <option value="youraccount__hiringManager">Student</option>
+                                <option value="youraccount__hiringManager">Curious</option>
+                            </select> 
+        </div>
+     
+       
+         <button type="button" className="waves-effect waves-light btn event__btn" onClick={this.handleSignUpButton}>Sign Up for Event </button> 
+    
          </Container>
      </div>
    );
