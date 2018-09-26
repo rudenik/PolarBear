e17a9f8a009269366f09f0fabed2271c6e0cc0ec
leandra <leandra.reid@gmail.com>
2018-09-26 13:47:21 -0400

    your account layout updates

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -16,12 +16,12 @@ class Landing extends Component {
         <h1 style={style} className="landing__name">Polar Bear</h1>
         <div className="landing__subscript">Your Networking Icebreaker</div>
         <div>
-             <a className="waves-effect btn-large landing__button">Sign Up</a>           
+             <a className="waves-effect btn-large landing__button">Sign Up</a>
          </div>
          <div>
             <a className="waves-effect btn-large landing__button">Log In</a>
         </div>
-        
+
     </div>
         )
     }
