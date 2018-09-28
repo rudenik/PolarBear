d3463904e324042c1a21132f2c9e9e435d63fa6b
leandra <leandra.reid@gmail.com>
2018-09-28 18:51:19 -0400

    youraccount changes

--- client/src/pages/YourAccount/YourAccount.js
+++ client/src/pages/YourAccount/YourAccount.js
@@ -3,6 +3,52 @@ import './YourAccount.css';
 import API from '../../utils/API';
 
 class YourAccount extends Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {
+            firstName: "",
+            lastName:"",
+            photo: "",
+            // jobTitle: "",
+            // jobStatus: "",
+            card1: "",
+            card2: "",
+            card3:""
+        }
+    }
+    handleChange = event => {
+
+    }
+    componentDidMount() {
+        API.getUserProfile(1)
+            // .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result.data);
+                    this.setState({
+                        firstName: result.data.firstName,
+                        lastName: result.data.lastName,
+                        photo: result.data.photo,
+                        // jobTitle: result.jobTitle,
+                        // jobStatus: res,
+                        card1: result.data.card1,
+                        card2: result.data.card2,
+                        card3: result.data.card3
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
   render() {
     return (
         <div className="youraccount__yourAccount youraccount__whiteBackground youracount__wrapper">
@@ -10,14 +56,16 @@ class YourAccount extends Component {
             <div className="youraccount__accountBox youraccount__orangeBackground">
                 <div className="youraccount__yourAccount youraccount__whiteBackground">
                     <div className="youraccount__flexRow">
-                        <img src = "http://www.placepuppy.net/1p/100/100" alt = "user headshot" className="youraccount__headShot"/ >
+                        <img src = {this.state.photo} alt = "user headshot" className="youraccount__headShot"/ >
                         <div className="youraccount__flexCol youraccount__nameInfo">
-                            <h3 className="youraccount__name youraccount__align-center youraccount__blackText youraccount__h3">Lorem Ipsum</h3>
-                            <h4 className="youraccount__currentTitle youraccount__align-center youraccount__blackText">Web Developer</h4>
+                            <h3 className="youraccount__name youraccount__align-center youraccount__blackText youraccount__h3">{this.state.firstName} {this.state.lastName}</h3>
+                            <h4 className="youraccount__currentTitle youraccount__align-center youraccount__blackText">{this.state.jobTitle}</h4>
                         </div>
                     </div>
 
 {/* //TODO: add axios calls to update each field */}
+
+{/* //TODO: if response includes card, make text box, add change button */}
                 <div className="youraccount__flexCol youraccount__userInfo">
                         <form action="#" className="youraccount__flexCol">
                             <label for="jobStatus" className="visuallyhidden">Job Status</label>
@@ -29,15 +77,15 @@ class YourAccount extends Component {
                                 <option value="youraccount__hiringManager">I'm a Hiring Manager</option>
                             </select>
                             <div className="input-field">
-                                <textarea id="skillsOne" class="materialize-textarea" data-length="140" maxlength="140"></textarea>
+                                <textarea id="skillsOne" class="materialize-textarea" data-length="140" maxLength="140">{this.state.card1}</textarea>
                                 <label for="skillsOne">Card One</label>
                             </div>
                             <div className="input-field">
-                                <textarea id="skillsTwo" class="materialize-textarea" data-length="140" maxlength="140"></textarea>
+                                <textarea id="skillsTwo" class="materialize-textarea" data-length="140" maxLength="140">{this.state.card2}</textarea>
                                 <label for="skillsTwo">Card Two</label>
                             </div>
                             <div className="input-field">
-                                <textarea id="skillsThree" class="materialize-textarea" data-length="140" maxlength="140"></textarea>
+                                <textarea id="skillsThree" class="materialize-textarea" data-length="140" maxLength="140"></textarea>
                                 <label for="skillsThree">Card Three</label>
                             </div>
 
